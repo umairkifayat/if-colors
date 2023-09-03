68fcde78218ef7e8e74ceef53baa6fbb798db0a2
umair kifayat <umairkifayat3@gmail.com>
2023-09-03 01:09:03 -0700

    if-colors

---
+++ app.js
@@ -0,0 +1,89 @@
+
+
+// function red(color) {
+//     document.body.style.backgroundColor = "red";
+// }
+// function blue(color) {
+//     document.body.style.backgroundColor = "blue";
+// }
+// function green(color) {
+//     document.body.style.backgroundColor = "green";
+// }
+// function orange(color) {
+//     document.body.style.backgroundColor = "orange";
+// }
+// function black(color) {
+//     document.body.style.backgroundColor = "black";
+// }
+
+
+
+
+
+
+// let arr = ["red", "blue", "green", "orange","black"];
+
+// function button(color) {
+// if (color === "red") {
+//     document.body.style.backgroundColor = "red";
+// }else if (color === "blue") {
+//     document.body.style.backgroundColor = "blue";
+// }else if (color === "green") {
+//     document.body.style.backgroundColor = "green";
+// }else if (color === "orange") {
+//     document.body.style.backgroundColor = "orange";
+// }else if (color === "black") {
+//     document.body.style.backgroundColor = "black";
+// }  
+// }
+
+
+let arr = ["red", "blue", "green", "orange", "black"];
+
+function button(color) {
+  if (color === "red") {
+    // Change the background color to red
+    document.body.style.backgroundColor = "red";
+  } else if (color === "blue") {
+    // Change the background color to blue
+    document.body.style.backgroundColor = "blue";
+  } else if (color === "green") {
+    // Change the background color to green
+    document.body.style.backgroundColor = "green";
+  } else if (color === "orange") {
+    // Change the background color to orange
+    document.body.style.backgroundColor = "orange";
+  } else if (color === "black") {
+    // Change the background color to black
+    document.body.style.backgroundColor = "black";
+  } else {
+    // Handle other cases or provide a default color
+    document.body.style.backgroundColor = "white"; // Default color is white
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Index = Math.floor(Math.random()*arr.length);
+// const arrColor = arr[Index]
+// document.body.style.backgroundColor = arrColor
